40f968592c265be8911a811a8bc5881f6055502a
bsyoo <bsyoo@bsnotebook.mshome.net>
2022-01-25 17:35:30 +0900

    homController @slf4추가

--- reservation/src/main/java/com/bs/reservation/controller/HomController.java
+++ reservation/src/main/java/com/bs/reservation/controller/HomController.java
@@ -3,7 +3,10 @@ package com.bs.reservation.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class HomController {
 	@RequestMapping("/")
 	public String home() {
